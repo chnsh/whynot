97290735b45a4ff03ad1c66a6a6438069ae91a6e
Smruthi Ramesh <rameshsmruthi@gmail.com>
2020-04-08 22:21:39 -0400

    fixing import error

--- whynot/simulators/covid19/__init__.py
+++ whynot/simulators/covid19/__init__.py
@@ -7,7 +7,7 @@ from whynot.simulators.covid19.simulator import (
     simulate,
     State,
 )
-from whynot.simulators.covid19.experiments import *
+from whynot.simulators.covid19.experiments import COVID19RCT
 from whynot.simulators.covid19.environments import *
 
 SUPPORTS_CAUSAL_GRAPHS = True
