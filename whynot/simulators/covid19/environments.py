fa4675a6bca0c3ab05ca8ac74e90cffce699bec2
Chintan Shah <chnsh24@gmail.com>
2020-04-19 17:50:23 -0400

    Changed action space and now the only possible action is to do social distancing

--- whynot/simulators/covid19/environments.py
+++ whynot/simulators/covid19/environments.py
@@ -1,5 +1,5 @@
 import numpy as np
-import itertools
+
 from whynot.gym import spaces
 from whynot.gym.envs import ODEEnvBuilder, register
 from whynot.simulators.covid19 import Config, Intervention, simulate, State
@@ -7,30 +7,31 @@ from whynot.simulators.covid19 import Config, Intervention, simulate, State
 
 def get_intervention(action, time):
     """Return the intervention in the simulator required to take action."""
-    scale_factor_val = action    
-    actions = [[0.9,0.75,0.5,0.0,Covid19Env.config.beta_scale_factor],\
-        [0.1,0.25,0.5,0.75,Covid19Env.config.proportion_hospitalized],\
-            [0.25,0.5,0.75,0.99,Covid19Env.config.proportion_recovered_after_hospitalization]]
-    actioncombos = list(itertools.product(*actions))
-    action_to_intervention_map = {i:actioncombos[i] for i in range(len(actioncombos))}
+    action_to_social_distancing_map = {
+        0: 1.0,
+        1: 0.75,
+        2: 0.5,
+        3: 0.25,
+        4: 0.10,
+        5: 0.0
+    }
 
-    beta_scale_factor,proportion_hospitalized,proportion_recovered_after_hospitalization = action_to_intervention_map[scale_factor_val]
+    beta_scale_factor = 1.0 - action_to_social_distancing_map[action]
+
     return Intervention(
         time=time,
-        beta_scale_factor=beta_scale_factor,
-        proportion_hospitalized=proportion_hospitalized,
-        proportion_recovered_after_hospitalization=proportion_recovered_after_hospitalization,
-        proportion_dead_after_hospitalization= 1 - proportion_recovered_after_hospitalization,
-        # proportion_recovered_without_hospitalization=proportion_recovered_without_hospitalization,
-        # proportion_dead_without_hospitalization=1 - proportion_recovered_without_hospitalization
+        beta_scale_factor=beta_scale_factor
     )
 
 
 def get_reward(intervention, state, time):
     """Compute the reward based on the observed state and choosen intervention."""
-    cost = 100000 * state.deceased + 10000 * state.hospitalized + 100 * state.exposed + 1000 * state.infected - 1 * state.recovered
-    discount = 4.0 / 365
-    return -cost + np.exp(discount * time)
+    C1 = 10
+
+    cost = 100 * state.infected
+    cost += C1 * (
+            1 - intervention.updates['beta_scale_factor'])  # cost of social distancing
+    return -cost
 
 
 def observation_space():
@@ -44,27 +45,11 @@ def observation_space():
     return spaces.Box(state_space_low, state_space_high, dtype=np.float64)
 
 
-# def action_space():
-#     """Return action space.
-
-#     There are three control variables in the model:
-#         - Set sigma
-#         - Set beta
-#         - Set mu
-
-#     """
-#     # return spaces.Tuple((spaces.Box(np.zeros(1), np.ones(3), dtype=np.int64),
-#     #                      spaces.Box(np.zeros(3), np.ones(3), dtype=np.float64)))
-#     return spaces.Box(np.zeros(1),3*np.ones(1),dtype=np.int64)
-    
-
-
 Covid19Env = ODEEnvBuilder(
     simulate_fn=simulate,
     config=Config(),
     initial_state=State(),
-    # action_space=action_space(),
-    action_space=spaces.Discrete(125),
+    action_space=spaces.Discrete(6),
     observation_space=observation_space(),
     timestep=1.0,
     intervention_fn=get_intervention,
